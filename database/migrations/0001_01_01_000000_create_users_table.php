3bd57abddd4211f5ceb366bb45f2aef83594cae6
Nicolas Carpita <nicolascarpita@macbookpro-2.home>
2024-10-05 10:41:17 +0200

    complétion table payss

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nom');
             $table->string('prenom');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('telephone');
             $table->string('adresse');
