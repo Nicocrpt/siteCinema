492fc7c0e65236ebe7dff17932f5abe03f4b446a
Nicolas Carpita <nicolascarpita@macbookpro-2.home>
2024-10-07 00:23:38 +0200

    ajout tailwind CSS au projet

--- resources/views/films/show.blade.php
+++ resources/views/films/show.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <div style="display:grid; grid-template-columns: 1fr 3fr; gap: 20px">
+    <div style="display:grid; grid-template-columns: 1fr 3fr; gap: 20px" class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50 m-3 rounded-2xl">
         <img src="{{ $film->url_affiche }}" alt="" style="width: 500px ; border-radius: 10px">
         <div>
-            <h1>{{ $film->titre }}</h1>
+            <h1 class="text-3xl mb-5 p-3">{{ $film->titre }}</h1>
             <p>Réalisateur : 
                 @foreach ($film->realisateurs as $realisateur)
                     @if ($realisateur === $film->realisateurs->last())
@@ -38,8 +39,19 @@
             @endforeach</p>
             <p> durée :{{$duration}}</p>
             <p>{{ $film->annee }}</p>   
-            <p>{{ $film->synopsis }}</p>
+            <p class="mb-10 mt-10">{{ $film->synopsis }}</p>
             <iframe width="560" height="315" src="{{ $film->url_trailer.'?modestbranding=1&controls=20&showinfo=0&rel=0' }}" frameborder="0" allowfullscreen style="border-radius: 10px; shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)]"></iframe>
+            <div class="pt-10 pb-10">
+                <h1 class="pb-5 text-xl">Séances</h1>
+                <div class="flex gap-10">
+                    @foreach ($film->seances as $seance)
+                        <a href="{{ route('seances.show', $seance->id)}}"><div class="flex flex-col justify-center items-center bg-amber-100 rounded-xl p-2">
+                            <p class="text-slate-600 font-bold">{{ date('d',strtotime($seance->datetime_seance)) }}</p>
+                            <p class="text-slate-600 font-bold">{{ date('H:i', strtotime($seance->datetime_seance))}}</p>
+                        </div></a>
+                    @endforeach
+                </div>
+            </div>
             <a href="{{ route('films.index') }}" class="rounded-sm underline hover:text-black focus:outline-none focus-visible:ring-1 focus-visible:ring-[#FF2D20] dark:hover:text-white">Retour</a>
         </div>
     </div>
