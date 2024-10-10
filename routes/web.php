2333abfcf880876bf7ccfc437102ae06c3c90411
Nicolas Carpita <nicolascarpita@macbookpro.home>
2024-10-10 23:54:43 +0200

    test intégration laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\SeanceController;
 use App\Http\Controllers\FilmController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SeanceController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +15,15 @@ Route::get('/seances', [SeanceController::class, 'index'])->name('seances.index'
 Route::get('/seances/{id}', [SeanceController::class, 'show'])->name('seances.show');
 
 Route::get('/my-account', [UserController::class, 'homepage'])->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
