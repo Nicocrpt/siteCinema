d55d503326fe571175942549dc7c4c69f6f7c099
Nicolas Carpita <nicolascarpita@macbookpro-8.home>
2025-02-09 22:42:49 +0100

    retest getAuth3

--- app/Http/Controllers/Admin/Api/AdminLoginController.php
+++ app/Http/Controllers/Admin/Api/AdminLoginController.php
@@ -13,17 +13,26 @@ class AdminLoginController extends Controller
     public function login(Request $request): JsonResponse
     {
         try {
+            $request->validate([
+                'name' => 'required',
+                'password' => 'required',
+            ]);
             if(Auth::attempt(['nom' => $request->name, 'password' => $request->password])){ 
+
                 if (Auth::user()->is_admin) {
                     $user = User::find(Auth::user()->id);
                     $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
                     $success['name'] =  $user->nom;
                     return response()->json(['success' => $success], 200);
+                } else {
+                    return response()->json(['error' => 'User find but Not Admin'], 401);
                 }
             
-            } 
+            } else {
+                return response()->json(['error' => 'User not found'], 401);
+            }
         }catch (\Exception $e) {
-            return response()->json(['error' => $e], 401);
+            return response()->json(['error' => $e->getMessage()], 401);
         }
     }
 
