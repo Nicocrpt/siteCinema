86e7dbbddf01605ec00625559c2ac8cf7ebd73de
Nicolas Carpita <nicolascarpita@macbookpro-8.home>
2025-02-09 22:26:40 +0100

    retest getAuth

--- app/Http/Controllers/Admin/Api/AdminLoginController.php
+++ app/Http/Controllers/Admin/Api/AdminLoginController.php
@@ -12,18 +12,19 @@ class AdminLoginController extends Controller
 {
     public function login(Request $request): JsonResponse
     {
-        if(Auth::attempt(['nom' => $request->name, 'password' => $request->password])){ 
-            if (Auth::user()->is_admin) {
-                $user = User::find(Auth::user()->id);
-                $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
-                $success['name'] =  $user->nom;
-                return response()->json(['success' => $success], 200);
-            }
-            return response()->json(['error'=>'Unauthorised'], 401);
-        } 
-        else{ 
-            return response()->json(['error'=>'Unauthorised'], 401);
-        } 
+        try {
+            if(Auth::attempt(['nom' => $request->name, 'password' => $request->password])){ 
+                if (Auth::user()->is_admin) {
+                    $user = User::find(Auth::user()->id);
+                    $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
+                    $success['name'] =  $user->nom;
+                    return response()->json(['success' => $success], 200);
+                }
+            
+            } 
+        }catch (\Exception $e) {
+            return response()->json(['error' => $e], 401);
+        }
     }
 
 }
