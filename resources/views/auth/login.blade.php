3ed27b9e5490b2d1feaa4c4c3dbcca87bb7eb644
Nicolas Carpita <nicolascarpita@Nicolass-MacBook-Pro.local>
2025-04-09 11:16:50 +0200

    modifications demandés modale de connexion #3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
-    <x-auth.login-form :registerLink="route('register')" />
+    <x-auth.login-form :registerLink="route('register')" :clickAction=""/>
 </x-guest-layout>
